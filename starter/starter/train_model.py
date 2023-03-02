1d85395cdab6b2fa672523bece96943fdc09ed85
Paulo Andrade <pauloeduardopandrade@gmail.com>
2023-03-01 23:06:43 -0300

    Add model training

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,12 +1,13 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
-# Add the necessary imports for the starter code.
+from ml.data import process_data
+from ml.model import train_model, save_model
 
-# Add code to load in the data.
+data = pd.read_csv('./data/census.csv')
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
@@ -23,6 +24,9 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    train, categorical_features=cat_features, label="salary", training=True
+)
 
-# Train and save a model.
+model = train_model(X_train, y_train)
+save_model(model, '../model/random_forest.pkl')
