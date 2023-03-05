8c7598bbb03b3f19649d30b4116c88bc82191178
Paulo Andrade <pauloeduardopandrade@gmail.com>
2023-03-05 13:26:30 -0300

    Complete model training and add pickles

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -4,9 +4,9 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 from ml.data import process_data
-from ml.model import train_model, save_model
+from ml.model import train_model, save_model, inference, compute_model_metrics
 
-data = pd.read_csv('./data/census.csv')
+data = pd.read_csv('./starter/data/census.csv')
 
 train, test = train_test_split(data, test_size=0.20)
 
@@ -24,9 +24,16 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-X_test, y_test, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
 )
 
 model = train_model(X_train, y_train)
-save_model(model, '../model/random_forest.pkl')
+
+preds = inference(model, X_test)
+
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+print(precision, recall, fbeta)
+save_model(model, './starter/model/random_forest.pkl')
+save_model(encoder, './starter/model/encoder.pkl')
+save_model(lb, './starter/model/label_binarizer.pkl')
